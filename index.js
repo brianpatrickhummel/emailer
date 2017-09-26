aeb47f86791cb8a0ff8ee36990089b3034e2a95e
brianpatrickhummel <frankgucci@mac.com>
2017-09-26 14:29:22 -0500

    updates

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const logger = require('morgan');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const logger = require("morgan");
+const keys = require('./config/keys');
 
 const app = express();
+
+// register the Google Strategy with Passport and create a new instance of this strategy
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+  })
+);
 
 // Run Morgan for Logging
 app.use(logger("dev"));
 
- 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
-
-const PORT = process.env.PORT || 5000;     // dynamic PORT binding
+// dynamic PORT binding to cover both local development and Heroku Deployment
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
