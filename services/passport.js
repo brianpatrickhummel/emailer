f39a4401513a3c69553de941d51cc9b759d8adee
brianpatrickhummel <frankgucci@mac.com>
2017-10-12 12:28:36 -0500

    modified google strategy to allow for proxy traffic

--- services/passport.js
+++ services/passport.js
@@ -24,7 +24,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     // Verify Callback when Google has returned the following account data items
     // done is a callback function
