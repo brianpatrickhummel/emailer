0113bf10f30f957f84560cf859b60c6c4ea6a813
brianpatrickhummel <frankgucci@mac.com>
2017-10-15 12:14:36 -0500

    restructure

--- server/services/passport.js
+++ server/services/passport.js
@@ -19,6 +19,9 @@ passport.deserializeUser((id, done) => {
 });
 
 // register the Google Strategy with Passport and create a new instance of this strategy
+// redirects browser to google for user to grant permission to our app
+// google will redirect to callbackURL and include user code
+// express server will retrieve user profile from Google and execute the callback below
 passport.use(
   new GoogleStrategy(
     {
@@ -29,18 +32,15 @@ passport.use(
     },
     // Verify Callback when Google has returned the following account data items
     // done is a callback function
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       // console.log('profile: ', profile);
       // All database queries are asynchronous, results are returned via a promise
-      User.findOne({ googleID: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleID: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+      const existingUser = await User.findOne({ googleID: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleID: profile.id }).save();
+      done(null, user);
     }
   )
 );
