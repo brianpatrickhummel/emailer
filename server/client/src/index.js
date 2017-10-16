5498d322b15fbf58d3956fb7047f36b04c4ac701
brianpatrickhummel <frankgucci@mac.com>
2017-10-16 14:56:13 -0500

    removed boilerplate react files from /src, began basic react/redux build, basic Auth reducer, combineReducers

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux"; // named exports.....{ name }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./components/App"; // a default export.....no braces
+import reducers from './reducers';
+
+// Create our Redux Store, pass in reducers, empty state object and the applyMiddleware method
+const store = createStore(reducers, {}, applyMiddleware());
+
+// Wrap our root level component in the Provider tag to connect Redux with React
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>, 
+  document.querySelector("#root")
+);
