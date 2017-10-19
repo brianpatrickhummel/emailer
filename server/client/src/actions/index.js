b2a87a38f9b7411c5dcd377b55acf391fc7f130a
brianpatrickhummel <frankgucci@mac.com>
2017-10-19 13:56:54 -0500

    created StripeCheckout component, embedded in Header component and adjusted logic, re-styled the Payment button, built action creator for POSTing Stripe token to Express API and dispatching action to modify state upon response, called this action creator from StripeCheckout component

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,8 +1,14 @@
 // action creators index.js
+
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
   dispatch({ type: FETCH_USER, payload: res.data }); // user profile info is stored in .data
+};
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post("/api/stripe", token);
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderContent() {
@@ -14,11 +15,14 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
-          <li>
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="2">
             <a href="/api/logout">LogOut</a>
           </li>
-        );
+        ];
     }
   }
 
@@ -29,10 +33,7 @@ class Header extends Component {
       <nav>
         {
           <div className="nav-wrapper">
-            <Link
-              to={this.props.auth ? "/surveys" : "/"}
-              className="left brand-logo"
-            >
+            <Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo">
               Emailer
             </Link>
             <ul className="right">{this.renderContent()}</ul>
