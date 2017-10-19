71c0189b50309ec6801750b28840fb93b7648e3f
brianpatrickhummel <frankgucci@mac.com>
2017-10-18 23:01:19 -0500

    Began coding for Stripe billing, installed react-stripe-checkout library, configured Stripe API keys server-side in dev.js and on Heroku, configured Stripe API key client-side as dev.prod React Env Variables (.env files)

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -5,7 +5,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux"; // named exports.....{ name }
-import reduxThunk from 'redux-thunk';
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App"; // a default export.....no braces
 import reducers from "./reducers";
