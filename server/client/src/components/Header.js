bd707319f6d59d27ac7f1c2db5eaf3b4e783eb50
brianpatrickhummel <frankgucci@mac.com>
2017-10-20 17:43:22 -0500

    created requiredLogin.js middleware to check login status of user when requests are received at /api/stripe, added a Header display of a user’s credits purchased

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -19,7 +19,10 @@ class Header extends Component {
           <li key="1">
             <Payments />
           </li>,
-          <li key="2">
+          <li key="2" style={{ margin: "0 10px" }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="3">
             <a href="/api/logout">LogOut</a>
           </li>
         ];
@@ -33,7 +36,7 @@ class Header extends Component {
       <nav>
         {
           <div className="nav-wrapper">
-            <Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo">
+            <Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo" style={{ margin: "0 10px" }}>
               Emailer
             </Link>
             <ul className="right">{this.renderContent()}</ul>
