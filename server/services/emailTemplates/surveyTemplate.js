1eddac4480c50cdca4c2f7855f18addaf973f521
brianpatrickhummel <frankgucci@mac.com>
2017-11-14 10:43:19 -0600

    add styling to email template

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -6,14 +6,17 @@ module.exports = survey => {
   return `
     <html>
       <body>
-        <div>
-          <h3 style="text-align: center">Your feedback is important to us!</h3>
-          <p style="text-align: center">We'd appreciate it you'd answer the following question:</p>
-          <h4 style="text-align: center">${survey.body}</h4>
+        <div style="background-color: rgba(1,1,1,0.6); padding: 25px; border: 1px solid white; border-radius: 10px;">
+          <h3 style="text-align: center; color: white;font-size:16px;">Your feedback is important to us!</h3>
+          <p style="text-align: center; color: white; font-size:14px; margin-bottom: 30px;">If you'd be willing to take a moment and respond to the following question we would greatly appreciate it:</p>
+          <h2 style="text-align: center; background-color: black; color: white;border: 1px solid white; padding: 20px;">${survey.body}</h2>
           <div style="text-align: center">
-            <a style="margin: 10px" href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">YES</a>
-
-            <a style="margin: 10px" href="${keys.redirectDomain}/api/surveys/${survey.id}/no">NO</a>
+            <button style="margin: 20px">
+              <a style="text-decoration: none" href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">YES</a>
+            </button>
+            <button style="margin: 20px; text-decoration: none;">
+              <a style="text-decoration: none"  href="${keys.redirectDomain}/api/surveys/${survey.id}/no">NO</a>
+            </button>
           </div>
         </div>
       </body>
